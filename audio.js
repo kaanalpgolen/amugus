22dedd6227967e649cfe4cf15bbca1ab5c19e30c
Kaan <kaan93265@gmail.com>
2025-10-03 21:34:58 +0300

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', function() {
-    var audio = document.getElementById('myAudio');
+    var audio = document.getElementById('madagascarpengu');
     if (audio) {
         audio.play().catch(function(error) {
             console.log('Oynatma hatası:', error);
         });
     }
 });
+
